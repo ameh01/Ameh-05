7c1f8a02c34cba2a452e05e293fdf5b723e75fb9
Friday Ameh Matthew <48910588+ameh01@users.noreply.github.com>
2022-04-25 22:23:48 +0100

    next config edited

--- next.config.js
+++ next.config.js
@@ -4,31 +4,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const sentryWebpackPluginOptions =
-  process.env.VERCEL_ENV === 'production'
-    ? {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
-        silent: false, // Logging when deploying to check if there is any problem
-        validate: true,
-        // Mark the release as Production
-        // https://github.com/getsentry/sentry-webpack-plugin/blob/master/src/index.js#L522
-        deploy: {
-          env: process.env.VERCEL_ENV,
-        },
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      }
-    : {
-        silent: true, // Suppresses all logs
-        dryRun: !process.env.SENTRY_AUTH_TOKEN,
-      }
+
 
 /** @type {import('next').NextConfig} */
 const config = {
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
   compiler: {
     styledComponents: true,
   },
@@ -101,4 +84,4 @@ const config = {
   },
 }
 
-module.exports = withBundleAnalyzer(withSentryConfig(config, sentryWebpackPluginOptions))
+module.exports = withBundleAnalyzer(withSentryConfig(config))
